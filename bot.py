a841d1adc3d0e6b81292f9ee5837db0a07a18354
lukasvasadi <lukas.vasadi@gmail.com>
2022-03-10 17:27:39 -0500

    first commit

---
+++ bot.py
@@ -0,0 +1,14 @@
+from email import message
+import os
+import slack
+import dotenv
+
+from pathlib import Path
+
+
+PATH_ENV = Path(".") / ".env"
+dotenv.load_dotenv(dotenv_path=PATH_ENV)
+
+client = slack.WebClient(token=os.environ["SLACK_TOKEN"])
+
+client.chat_postMessage(channel="#gbg-updates", text="Hello World!")
